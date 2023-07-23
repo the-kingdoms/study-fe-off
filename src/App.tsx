d2f4918ac2cdeb03177b4526087b5bf900f8795c
VsangkyeongV <137258368+VsangkyeongV@users.noreply.github.com>
2023-07-23 15:48:25 +0900

    secondpage

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
 import React from "react";
 import SignInForm from "./components/SignInForm";
+import CreateAccount from "./components/CreateAccount";
+import { Route, Routes } from "react-router-dom";
 function App() {
-  return <SignInForm />;
+  return (
+    <Routes>
+      <Route path="/" element={<SignInForm />} />
+      <Route path="/2" element={<CreateAccount />} />
+    </Routes>
+  );
 }
 
 export default App;
--- src/components/CreateAccount.tsx
+++ src/components/CreateAccount.tsx
@@ -1,22 +1,26 @@
-import BlueButton from "./UI/BlueButton";
 import InputField from "./UI/InputField";
+import BlueButton from "./UI/BlueButton";
 import { Link } from "react-router-dom";
-const SignInForm = () => {
+
+const CreateAccount = () => {
   return (
     <div
       className="flex flex-col
-     justify-center items-center"
+ justify-center items-center"
     >
       <div className="w-[800px] h-[40px] flex flex-col justify-center items-center text-6xl font-bold">
-        sign in
+        Create Account
       </div>
       <form>
-        <div className="w-[800px] h-[263px] flex flex-col justify-center items-center mt-[60px]">
+        <div className="w-[800px] h-[276px] flex flex-col justify-center items-center mt-[60px]">
           <div className="w-[600px] h-[48px] ">
             <InputField text="Email" />
           </div>
           <div className="w-[600px] h-[48px] mt-[24px]">
             <InputField text="Password" />
+          </div>
+          <div className="w-[600px] h-[48px] mt-[24px]">
+            <InputField text="Password Confirm" />
           </div>
           <div className="w-[600px] h-[60px] mt-[24px]">
             <BlueButton
@@ -26,13 +30,8 @@ const SignInForm = () => {
           </div>
         </div>
       </form>
-      <div className="flex flex-row justify-center items-center mt-[19px]">
-        <div className="mr-[10px]">Don’t have an account? </div>
-        <Link to={"/2"} className="text-sky-400">
-          Sign Up Here
-        </Link>
-      </div>
     </div>
   );
 };
-export default SignInForm;
+
+export default CreateAccount;
