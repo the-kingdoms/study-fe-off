6091b27eed6fcf98ca6c1d4f5ab378d17e26bdc6
Developerryou <developerryou@gmail.com>
2023-07-23 17:06:14 +0900

    FEAT: 게시글 확인 페이지 추가

--- src/components/post/PostContent.tsx
+++ src/components/post/PostContent.tsx
@@ -1,5 +1,21 @@
-function PostContent() {
-  return <></>;
+import PostInterface from "../../interfaces/PostInterface";
+import FlexColumn from "../basic/FlexColumn";
+import FlexRow from "../basic/FlexRow";
+
+function PostContent({ post }: { post: PostInterface }) {
+  return (
+    <FlexColumn>
+      <div className="w-full font-boldd mb-4">{post.title}</div>
+      <FlexRow className="w-full mb-4 justify-between">
+        <div className="font-bold text-blue-500">{post.user_name}</div>
+        <div className="">{post.created_date}</div>
+      </FlexRow>
+      <div
+        className="w-full mb-4"
+        dangerouslySetInnerHTML={{ __html: post.content }}
+      ></div>
+    </FlexColumn>
+  );
 }
 
 export default PostContent;
